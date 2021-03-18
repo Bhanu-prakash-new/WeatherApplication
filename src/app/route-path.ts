adfb88e07d8d7353fec462296b8d353515f0d005
Bhanu Prakash Kopparthi (Innominds Sofware Inc) <v-bhkop@microsoft.com>
2021-03-19 01:04:23 +0530

    comment removed

--- src/app/route-path.ts
+++ src/app/route-path.ts
@@ -1,10 +1,3 @@
-
-// -----------------------------------------------------------------------
-// <copyright company="Microsoft Corporation">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// ----------------------------------------------------------------------
-
 export class RoutePath {
   public static weather = 'weather';
   public static weatherDetail = 'weatherDetail/:city';
